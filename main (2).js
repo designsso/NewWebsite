0268654adeaeb4215394da0b88bc560ee7f5ab50
designsso <111659494+designsso@users.noreply.github.com>
2022-08-26 16:27:51 +0200

    Add files via upload

---
+++ main (2).js
@@ -0,0 +1,116 @@
+console.log(screen.width)
+$('#sub').hover(() => {
+	if(screen.width < 1400) {
+		$('#sub').animate({
+			marginTop: '-500px',
+			scrollTop: 0,
+			height: $('#main').height() + 'px'
+		}, 0);
+	// } else if(screen.width > 1400) {
+	// 	$('#sub').animate({
+	// 		marginTop: '-700px',
+	// 		scrollTop: 0,
+	// 		height: $('#main').height() + 'px'
+	// 	}, 0);
+	} else {
+		$('#sub').animate({
+			marginTop: '-700px',
+			scrollTop: 0,
+			height: $('#main').height() + 'px'
+		}, 0);
+	}
+
+	$('#sub').css('overflow', 'auto')
+
+	$('.btn-group-vertical').animate({
+		marginTop: '-75px',
+		scrollTop: '0px'
+	}, 25);
+
+	setTimeout(() => {
+		$('#submain').css('visibility', 'hidden');
+	}, 100);
+}, () => {
+	if(screen.width < 1400) {
+		$('#sub').animate({
+			marginTop: '-150px',
+			scrollTop: '0px',
+			height: '150px'
+		}, 0);
+	// } else if(screen.width > 1400) {
+	// 	$('#sub').animate({
+	// 		marginTop: '-500px',
+	// 		scrollTop: '0px',
+	// 		height: '175px'
+	// 	}, 0);
+	}  else {
+		$('#sub').animate({
+			marginTop: '-175px',
+			scrollTop: '0px',
+			height: '175px'
+		}, 0);
+	}
+
+	$('.btn-group-vertical').animate({
+		marginTop: '100px',
+		scrollTop: '0px'
+	}, 25);
+
+	setTimeout(() => {
+		$('#submain').css('visibility', 'visible');
+	}, 100);
+})
+
+$(document).ready(function(){
+    $('#btnup').on('click', () => {
+		if(screen.width < 1400) {
+			$('#sub').animate({
+				marginTop: '-500px',
+				scrollTop: 0,
+				height: $('#main').height() + 'px'
+			}, 0);
+		} else {
+			$('#sub').animate({
+				marginTop: '-700px',
+				scrollTop: 0,
+				height: $('#main').height() + 'px'
+			}, 0);
+		}
+
+		$('#sub').css('overflow', 'auto')
+
+		$('.btn-group-vertical').animate({
+			marginTop: '-75px',
+			scrollTop: '0px'
+		}, 150);
+
+		setTimeout(() => {
+			$('#submain').css('visibility', 'hidden');
+		}, 300);
+    })
+
+    $('#btndown').on('click', () => {
+		if(screen.width < 1400) {
+			$('#sub').animate({
+				marginTop: '-150px',
+				scrollTop: '0px',
+				height: '150px'
+			}, 0);
+		} else {
+			$('#sub').animate({
+				marginTop: '-175px',
+				scrollTop: '0px',
+				height: '175px'
+			}, 0);
+		}
+
+		$('.btn-group-vertical').animate({
+			marginTop: '100px',
+			scrollTop: '0px'
+		}, 150);
+
+		setTimeout(() => {
+			$('#submain').css('visibility', 'visible');
+		}, 300);
+    })
+});
