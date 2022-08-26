f0934b3aa93b51b32e49f58fc4f570f7bf0f037a
designsso <111659494+designsso@users.noreply.github.com>
2022-08-26 02:07:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $('#sub').hover(() => {
 	$('#sub').animate({
-		marginTop: '-550px',
+		marginTop: '-700px',
 		scrollTop: 0,
 		height: $('#main').height() + 'px'
 	}, 0);
@@ -31,46 +31,11 @@ $('#sub').hover(() => {
 		$('#submain').css('visibility', 'visible');
 	}, 100);
 })
-// $('body').on('mousewheel', function(e){
-// 	if(screen.width > 600 ) {
-// 		if(e.originalEvent.wheelDelta > 0) {
-// 			$('#sub').animate({
-// 				marginTop: '-550px',
-// 				scrollTop: 0,
-// 				height: $('#main').height() + 'px'
-// 			}, 0);
-
-// 			$('.btn-group-vertical').animate({
-// 				marginTop: '-75px',
-// 				scrollTop: '0px'
-// 			}, 25);
-
-// 			setTimeout(() => {
-// 				$('#submain').css('visibility', 'hidden');
-// 			}, 100);
-// 		} else {
-// 			$('#sub').animate({
-// 				marginTop: '-175px',
-// 				scrollTop: '0px',
-// 				height: '175px'
-// 			}, 0);
-
-// 			$('.btn-group-vertical').animate({
-// 				marginTop: '100px',
-// 				scrollTop: '0px'
-// 			}, 25);
-
-// 			setTimeout(() => {
-// 				$('#submain').css('visibility', 'visible');
-// 			}, 0);
-// 		}
-// 	}
-// });
 
 $(document).ready(function(){
     $('#btnup').on('click', () => {
     	$('#sub').animate({
-			marginTop: '-550px',
+			marginTop: '-700px',
 			scrollTop: 0,
 			height: ($('#main').height() + 3 ) + 'px'
 		}, 0);
