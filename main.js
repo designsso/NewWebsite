2aabbaf320fe10b667691bdb31c8f4a8fa938c69
designsso <111659494+designsso@users.noreply.github.com>
2022-08-27 22:56:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,104 +1,80 @@
 console.log(screen.width)
 $('#sub').hover(() => {
-	if(screen.width <= 1440) {
+	if(screen.width >= 767) {
 		$('#sub').animate({
-			marginTop: '-500px',
+			marginTop: '-800px',
 			scrollTop: 0,
 			height: $('#main').height() + 'px'
 		}, 0);
-	} else {
-		$('#sub').animate({
-			marginTop: '-700px',
-			scrollTop: 0,
-			height: $('#main').height() + 'px'
-		}, 0);
-	}
-
-	$('#sub').css('overflow', 'auto')/Users/louis289/Downloads/NewWebsite-main/main.js
-
-	$('.btn-group-vertical').animate({
-		marginTop: '-75px',
-		scrollTop: '0px'
-	}, 25);
-
-	setTimeout(() => {
-		$('#submain').css('visibility', 'hidden');
-	}, 100);
-}, () => {
-	if(screen.width < 1440) {
-		$('#sub').animate({
-			marginTop: '-150px',
-			scrollTop: '0px',
-			height: '150px'
-		}, 0);
-	}  else {
-		$('#sub').animate({
-			marginTop: '-175px',
-			scrollTop: '0px',
-			height: '175px'
-		}, 0);
-	}
-
-	$('.btn-group-vertical').animate({
-		marginTop: '100px',
-		scrollTop: '0px'
-	}, 25);
-
-	setTimeout(() => {
-		$('#submain').css('visibility', 'visible');
-	}, 100);
-})
-
-$(document).ready(function(){
-    $('#btnup').on('click', () => {
-		if(screen.width <= 1440) {
-			$('#sub').animate({
-				marginTop: '-500px',
-				scrollTop: 0,
-				height: $('#main').height() + 'px'
-			}, 0);
-		} else {
-			$('#sub').animate({
-				marginTop: '-700px',
-				scrollTop: 0,
-				height: $('#main').height() + 'px'
-			}, 0);
-		}
 
 		$('#sub').css('overflow', 'auto')
 
 		$('.btn-group-vertical').animate({
 			marginTop: '-75px',
 			scrollTop: '0px'
-		}, 150);
+		}, 50);
 
 		setTimeout(() => {
 			$('#submain').css('visibility', 'hidden');
-		}, 300);
+		}, 125);
+	}
+}, () => {
+	if(screen.width >= 767) {
+		$('#sub').animate({
+			marginTop: '-175px',
+			scrollTop: '0px',
+			height: '175px'
+		}, 0);
+
+		$('.btn-group-vertical').animate({
+			marginTop: '100px',
+			scrollTop: '0px'
+		}, 50);
+
+		setTimeout(() => {
+			$('#submain').css('visibility', 'visible');
+		}, 125);
+	}
+})
+
+$(document).ready(function(){
+    $('#btnup').on('click', () => {
+		if(screen.width >= 767) {
+			$('#sub').animate({
+				marginTop: '-800px',
+				scrollTop: 0,
+				height: $('#main').height() + 'px'
+			}, 0);
+
+			$('#sub').css('overflow', 'auto')
+
+			$('.btn-group-vertical').animate({
+				marginTop: '-75px',
+				scrollTop: '0px'
+			}, 50);
+
+			setTimeout(() => {
+				$('#submain').css('visibility', 'hidden');
+			}, 125);
+		}
     })
 
     $('#btndown').on('click', () => {
-		if(screen.width < 1440) {
-			$('#sub').animate({
-				marginTop: '-150px',
-				scrollTop: '0px',
-				height: '150px'
-			}, 0);
-		} else {
+    	if(screen.width >= 767) {
 			$('#sub').animate({
 				marginTop: '-175px',
 				scrollTop: '0px',
 				height: '175px'
 			}, 0);
+
+			$('.btn-group-vertical').animate({
+				marginTop: '100px',
+				scrollTop: '0px'
+			}, 50);
+
+			setTimeout(() => {
+				$('#submain').css('visibility', 'visible');
+			}, 125);
 		}
-
-		$('.btn-group-vertical').animate({
-			marginTop: '100px',
-			scrollTop: '0px'
-		}, 150);
-
-		setTimeout(() => {
-			$('#submain').css('visibility', 'visible');
-		}, 300);
     })
 });
